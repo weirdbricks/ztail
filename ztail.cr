432232936ea2deab8978d1189c32eede9004b6ce
Lampros Chaidas <lampros@chaidas.com>
2019-06-03 12:40:48 -0700

    made the script more resilient

--- ztail.cr
+++ ztail.cr
@@ -1,22 +1,23 @@
 require "gzip"
 
 if ARGV.size == 0
-	puts "You need to provide a filename"
+	puts "Ztail: You need to provide a filename"
 	exit 1
 end
 
 if ARGV.size > 1
-	puts "You only need to provide one filename as an argument"
+	puts "Ztail: You only need to provide one filename as an argument"
 	exit 1
 end
 
 filename = ARGV[0]
 
 unless File.file?(filename)
-	puts "Sorry, but I cannot find the file \"#{filename}\""
+	puts "Ztail: Sorry, but I cannot find the file \"#{filename}\""
 	exit 1
 end
 
+# set an initial counter
 counter=0
 
 # go through the file once and count the number of lines
@@ -28,7 +29,7 @@ begin
 		end
 	end
 rescue
-	puts "Sorry, but I cannot open the file - is this a gzip compressed file?"
+	puts "Ztail: Sorry, but I cannot open the file - is this a gzip compressed file?"
 	exit 1
 end
 
@@ -39,28 +40,26 @@ else
 	last_line = counter - 10	
 end
 
-# set an initial counter
 counter=0
-
-file_size=File.size(filename)
-# skip to the middle of the file
-skip_to=(file_size/2).to_i32
 
 # we need to re-read the gzip header to get the new lines - this isn't efficient but it works
 loop do
 	# if we don't sleep here we'll end up killing the cpu 
 	# as it will retry as fast as possible
 	sleep 0.5
-	Gzip::Reader.open(filename) do |gzip|
-		gzip.skip(skip_to)
-		gzip.each_line do |line|
-			counter=counter+1
-			# only show lines that are newer than our counter
-			if counter > last_line
-				puts line
-			end
-	        end
-		last_line=counter
-		counter = 0
+	begin 
+		Gzip::Reader.open(filename) do |gzip|
+			gzip.each_line do |line|
+				counter=counter+1
+				# only show lines that are newer than our counter
+				if counter > last_line
+					puts line
+				end
+		        end
+			last_line=counter
+			counter = 0
+		end
+	rescue exception
+		puts "Ztail: Something went wrong while reading the file!: Exception: #{exception.message}"
 	end
 end
